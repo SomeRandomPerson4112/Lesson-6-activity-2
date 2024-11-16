89d634773eb3cb5e55ca5eabf4b9866c42540841
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-16 19:02:27 +0500

    main.125372938452018364524t82

---
+++ Not operator.py
@@ -0,0 +1,12 @@
+a=5
+b=10
+c=10
+print(a!=b)
+print(b!=c)
+a="python"
+b="coding"
+if a !=b:
+    print(a,"and",b,"are different")
+a=int(input("enter a number: "))
+if a%2!=0:
+    print(a,"is an odd number")
